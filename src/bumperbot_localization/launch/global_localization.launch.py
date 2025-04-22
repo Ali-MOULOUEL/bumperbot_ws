d53115f92efe44ea04c2ab66bfa1395ed30bac07
Ali MOULOUEL <ali.moulouel@gmail.com>
2025-04-22 15:12:11 +0200

    Add nav2_bringup launch integration to main launch file

--- src/bumperbot_localization/launch/global_localization.launch.py
+++ src/bumperbot_localization/launch/global_localization.launch.py
@@ -4,6 +4,8 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
 
 def generate_launch_description():
 
@@ -70,11 +72,26 @@ def generate_launch_description():
         ]
     )
 
+    nav2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'launch',
+                'navigation_launch.py'
+            )
+        ]),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'map': map_path
+        }.items()
+    )
+
     return LaunchDescription([
         map_name_arg,
         use_sim_time_arg,
         amcl_config_arg,
         nav2_map_server,
         nav2_amcl,
-        nav2_lifecycle_manager
+        nav2_lifecycle_manager,
+        nav2_bringup
     ])
